4b536a06af20354c3d88b6473a20d23dd68f55a9
Jachou-yt <matisse.martayan@gmail.com>
2025-06-19 17:55:45 +0200

    Initial commit

---
+++ src/main/java/fr/jachou/topluck/Topluck.java
@@ -0,0 +1,17 @@
+package fr.jachou.topluck;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Topluck extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
